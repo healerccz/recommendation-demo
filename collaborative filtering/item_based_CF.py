f86d200b7934b0bfd9c5e3ae6a4857605237190d
zhaochengcheng <15955702565@163.com>
2020-11-11 21:21:58 +0800

    feat: create item_based_CF

---
+++ collaborative filtering/item_based_CF.py
@@ -0,0 +1,11 @@
+"""
+@description: a demo for item_based collaborative filtering
+@author: Chengcheng Zhao
+@date: 2020-11-11 21:19:00
+"""
+import random
+import operator
+
+class ItemBasedCF:
+    def __init__(self):
+        self.
